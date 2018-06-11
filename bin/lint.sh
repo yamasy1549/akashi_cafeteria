4e4b7f7bd8102e95f69f70b25166ac63935e2e55
yamasy1549 <sanae@yamasy.info>
2018-06-11 13:15:18 +0900

    php-cs-fixer追加

---
+++ bin/lint.sh
@@ -0,0 +1,13 @@
+if [ `which php-cs-fixer` ]; then
+    php-cs-fixer fix public_html/*.php $*
+else
+    echo 'Command `php-cs-fixer` is not found.'
+    echo 'Please install.'
+    echo 'ref: https://github.com/FriendsOfPHP/PHP-CS-Fixer'
+    echo '==========================='
+    echo ' $ curl -L https://cs.sensiolabs.org/download/php-cs-fixer-v2.phar \'
+    echo '   -o php-cs-fixer'
+    echo ' $ sudo chmod a+x php-cs-fixer'
+    echo ' $ sudo mv php-cs-fixer /usr/local/bin/php-cs-fixer'
+    echo '==========================='
+fi
