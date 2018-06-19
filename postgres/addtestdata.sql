95480d43d64fca62660345410dbdc5dc2bec0578
yamasy1549 <sanae@yamasy.info>
2018-06-19 12:41:32 +0900

    テーブル生成、テストデータ追加のSQL

--- postgres/addtestdata.sql
+++ postgres/addtestdata.sql
@@ -1,4 +1,39 @@
+/* まだ集合が分からないので手打ちデータを入力しています　*/
+/* TODO: 変更が容易なデータの作成（関数などを用いる）　*/
+/* TODO; コマンドラインで確認する */
+
 insert into "user" (email, password, role) values
     ('test1@example.com', 'password', '管理者'),
     ('test2@example.com', 'password', '管理者'),
     ('test3@example.com', 'password', '評価者');
+
+insert into category (name) values
+    ('麺類'),
+    ('Aセット'),
+    ('Bセット');
+
+insert into menu (category_id, name, price, image) values
+    ('1', '醤油ラーメン', 210, 'https://akashi_cafeteria/image/056'),
+    ('2', '親子丼', 360, 'https://akashi_cafeteria/image/023'),
+    ('3', 'タコライス', 360, 'https://akashi_cafeteria/image/034');
+
+insert into evaluation (user_id, menu_id, data) values
+    ('1', '1', '5'),
+    ('2', '2', '4'),
+    ('3', '3', '5');
+
+insert into daymenu (date, menu_id, sale, evaluation_id) values
+    ('2018-06-12', '1', true, '1'),
+    ('2018-06-13', '2', true, '2'),
+    ('2018-06-14', '3', false, '3');
+
+/* 集合で作ってみたかったんです！
+insert into "user" (email, password, role) values
+select
+  format('test%s@example.com', i),
+  format('password'),
+
+from
+  generate_series(1, 10) as i
+;
+*/
--- postgres/createtable.sql
+++ postgres/createtable.sql
@@ -15,15 +15,40 @@ create type role as enum (
 );
 
 /* userは予約語なのでダブルクォーテーションが必要みたい */
-/* TODO: 一意制約などをつける */
+/* TODO; コマンドラインで確認する */
+
 create table "user" (
-    id          serial,
-    email       text,
-    password    text,
-    role        role
+    user_id     serial primary key,
+    email       text not null,
+    password    text not null,
+    role        role not null
 );
 
-/* TODO: create table evaluation */
-/* TODO: create table category */
-/* TODO: create table menu */
-/* TODO: create table daymenu */
+create table category (
+    category_id serial primary key,
+    name        text not null
+);
+
+create table menu (
+    menu_id     serial primary key,
+    category_id integer references category(category_id) not null,
+    name        text not null,
+    price       integer not null,
+    image       text not null
+);
+
+create table evaluation (
+    evaluation_id    serial primary key,
+    user_id          integer references "user"(user_id) not null,
+    menu_id          integer references menu(menu_id) not null,
+    data             integer not null
+);
+
+/* sale: true...販売中　false...売り切れ　*/
+create table daymenu (
+    daymanu_id    serial primary key,
+    date          date not null,
+    menu_id       integer references menu(menu_id) not null,
+    sale          boolean default true not null,
+    evaluation_id integer references evaluation(evaluation_id) not null
+);
