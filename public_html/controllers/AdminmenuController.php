59f7ee62faf26f61307a820dc979473d53f9b892
yamasy1549 <sanae@yamasy.info>
2018-07-03 11:49:52 +0900

    使い方のページを作成（未完成）

--- public_html/controllers/AdminmenuController.php
+++ public_html/controllers/AdminmenuController.php
@@ -28,4 +28,17 @@ class AdminmenuController
         // テンプレート表示
         $this->view->display('./views/adminmenu/index.tpl');
     }
+
+    /**
+     * howtouse
+     */
+    public function howtouseAction()
+    {
+        // テンプレートへ変数割り当て
+        $this->view->assign('button_action', 'controller=daymenu&action=index');
+        $this->view->assign('button_name', '日毎メニュー管理');
+
+        // テンプレート表示
+        $this->view->display('./views/adminmenu/howtouse.tpl');
+    }
 }
