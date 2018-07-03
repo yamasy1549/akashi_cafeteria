c6667b607ae5fb92ab38dd0bd2d98739c71fdc54
nakata_syo <e1428@s.akashi.ac.jp>
2018-07-03 11:31:24 +0900

    Add files via upload

---
+++ public_html/models/Daymenu.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once './models/BaseModel.php';
+
+/**
+ * カテゴリモデル
+ */
+class Daymenu extends BaseModel
+{
+    private $model_name = 'daymenu';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    * DBから取得（全部）
+    */
+    public function getDaymenus()
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('select * from %s', $this->model_name);
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    /**
+    * DBから取得（一部）
+    */
+    public function getDaymenu($daymenu_id)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('select * from %s where daymenu_id = %s', $this->model_name, $daymenu_id);
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
+
+    /**
+    * データ更新
+    */
+    public function update($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf("update %s set name = '%s' where daymenu_id = %s", $this->model_name, $params['name'], $params['daymenu_id']);
+        $res = $this->db->query($sql);
+    }
+
+    /**
+    * データ追加
+    */
+    public function create($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf("insert into %s (name) values ('%s')", $this->model_name, $params['name']);
+        $res = $this->db->query($sql);
+    }
+
+    /**
+    * データ削除
+    */
+    public function destroy($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('delete from %s where daymenu_id = %s', $this->model_name, $params['daymenu_id']);
+        $res = $this->db->query($sql);
+    }
+}
