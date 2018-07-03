6c8d8998800c9a88401ee06caf17406a30a00d6f
mukkoro <37657175+Kawahara419@users.noreply.github.com>
2018-07-03 11:00:30 +0900

    Add files via upload

---
+++ public_html/controllers/EvaluationController.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once './Request.php';
+require_once './models/Evaluation.php';
+require_once './views/smarty/Smarty.class.php';
+
+class EvaluationController
+{
+    private $view;
+    private $request;
+    private $model;
+
+    /**
+     * コンストラクタ
+     */
+    public function __construct()
+    {
+        $this->view = new Smarty();
+        $this->view->template_dir = '../view/templates';
+
+        $this->request = new Request();
+
+        $this->model = new Evaluation();
+    }
+
+    /**
+     * index
+     */
+    public function indexAction()
+    {
+        $categories = $this->model->getCategories();
+
+        // テンプレートへ変数割り当て
+        $this->view->assign('categories', $evaluations);
+        $this->view->assign('button_action', 'controller=evaluation&action=new');
+        $this->view->assign('button_name', '評価追加');
+
+        // テンプレート表示
+        $this->view->display('./views/evaluation/index.tpl');
+    }
+
+    /**
+     * new
+     */
+    public function newAction()
+    {
+        // テンプレートへ変数割り当て
+        $this->view->assign('button_action', 'controller=evaluation&action=new');
+        $this->view->assign('button_name', 'カテゴリ追加');
+
+        // テンプレート表示
+        $this->view->display('./views/evaluation/new.tpl');
+    }
+
+    /**
+     * create
+     */
+    public function createAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->create($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=evaluation&action=index');
+        exit();
+    }
+
+    /**
+     * edit
+     */
+    public function editAction()
+    {
+        $params = $this->request->getQuery();
+        $evaluation = $this->model->getEvaluation($params['evaluation_id']);
+
+        // テンプレートへ変数割り当て
+        $this->view->assign('evaluation', $evaluation);
+        $this->view->assign('button_action', 'controller=evaluation&action=new');
+        $this->view->assign('button_name', 'カテゴリ追加');
+
+        // テンプレート表示
+        $this->view->display('./views/evaluation/edit.tpl');
+    }
+
+    /**
+     * update
+     */
+    public function updateAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->update($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=evaluation&action=index');
+        exit();
+    }
+
+    /**
+     * delete
+     */
+    public function deleteAction()
+    {
+        $params = $this->request->getQuery();
+        $evaluation = $this->model->getEvaluation($params['evaluation_id']);
+
+        // テンプレートへ変数割り当て
+        $this->view->assign('evaluation', $evaluation);
+        $this->view->assign('button_action', 'controller=evaluation&action=new');
+        $this->view->assign('button_name', '評価追加');
+
+        // テンプレート表示
+        $this->view->display('./views/evaluation/delete.tpl');
+    }
+
+    /**
+     * destroy
+     */
+    public function destroyAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->destroy($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=evaluation&action=index');
+        exit();
+    }
+}
