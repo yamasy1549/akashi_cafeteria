cb8a3ca2527d1b498c21db1f3fb0b9dd59254db8
mukkoro <37657175+Kawahara419@users.noreply.github.com>
2018-07-03 11:00:06 +0900

    Add files via upload

---
+++ public_html/models/Evaluation.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once './models/BaseModel.php';
+
+/**
+ * カテゴリモデル
+ */
+class Evaluation extends BaseModel
+{
+    private $model_name = 'evaluation';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    * DBから取得（全部）
+    */
+    public function getEvalations()
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('select * from %s', $this->model_name);
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    /**
+    * DBから取得（一部）
+    */
+    public function getEvaluation($evaluation_id)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('select * from %s where evaluation_id = %s', $this->model_name, $evaluation_id);
+        $stmt = $this->db->query($sql);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
+
+    /**
+    * データ更新
+    */
+    public function update($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf("update %s set name = '%s' where evaluation_id = %s", $this->model_name, $params['name'], $params['evaluation_id']);
+        $res = $this->db->query($sql);
+    }
+
+    /**
+    * データ追加
+    */
+    public function create($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf("insert into %s (name) values ('%s')", $this->model_name, $params['name']);
+        $res = $this->db->query($sql);
+    }
+
+    /**
+    * データ削除
+    */
+    public function destroy($params)
+    {
+        // TODO: セキュリティ対策
+        // TODO: エラーハンドリング
+        $sql = sprintf('delete from %s where evaluation_id = %s', $this->model_name, $params['evaluation_id']);
+        $res = $this->db->query($sql);
+    }
+}
