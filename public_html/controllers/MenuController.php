5ce64aa1c346a47cd41b874805e3193fe0379f1a
yamasy1549 <sanae@yamasy.info>
2018-07-07 12:20:13 +0900

    Menu 正常系実装

--- public_html/controllers/MenuController.php
+++ public_html/controllers/MenuController.php
@@ -7,6 +7,8 @@ require_once './views/smarty/Smarty.class.php';
 class MenuController
 {
     private $view;
+    private $request;
+    private $model;
 
     /**
      * コンストラクタ
@@ -15,20 +17,114 @@ class MenuController
     {
         $this->view = new Smarty();
         $this->view->template_dir = '../view/templates';
+
+        $this->request = new Request();
+
+        $this->model = new menu();
     }
 
+    /**
+     * index
+     */
     public function indexAction()
     {
-        // $request = new Request();
-        // $params = $request->getQuery();
-
-        $menu_model = new Menu();
-        $menus = $menu_model->getMenus();
+        $menus = $this->model->getmenus();
 
         // テンプレートへ変数割り当て
         $this->view->assign('menus', $menus);
+        $this->view->assign('button_action', 'controller=menu&action=new');
+        $this->view->assign('button_name', 'メニュー追加');
 
         // テンプレート表示
         $this->view->display('./views/menu/index.tpl');
+    }
+
+    /**
+     * new
+     */
+    public function newAction()
+    {
+        // テンプレートへ変数割り当て
+        $this->view->assign('button_action', 'controller=menu&action=new');
+        $this->view->assign('button_name', 'メニュー追加');
+
+        // テンプレート表示
+        $this->view->display('./views/menu/new.tpl');
+    }
+
+    /**
+     * create
+     */
+    public function createAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->create($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=menu&action=index');
+        exit();
+    }
+
+    /**
+     * edit
+     */
+    public function editAction()
+    {
+        $params = $this->request->getQuery();
+        $menu = $this->model->getmenu($params['menu_id']);
+
+        // テンプレートへ変数割り当て
+        $this->view->assign('menu', $menu);
+        $this->view->assign('button_action', 'controller=menu&action=new');
+        $this->view->assign('button_name', 'メニュー追加');
+
+        // テンプレート表示
+        $this->view->display('./views/menu/edit.tpl');
+    }
+
+    /**
+     * update
+     */
+    public function updateAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->update($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=menu&action=index');
+        exit();
+    }
+
+    /**
+     * delete
+     */
+    public function deleteAction()
+    {
+        $params = $this->request->getQuery();
+        $menu = $this->model->getmenu($params['menu_id']);
+
+        // テンプレートへ変数割り当て
+        $this->view->assign('menu', $menu);
+        $this->view->assign('button_action', 'controller=menu&action=new');
+        $this->view->assign('button_name', 'カテゴリ追加');
+
+        // テンプレート表示
+        $this->view->display('./views/menu/delete.tpl');
+    }
+
+    /**
+     * destroy
+     */
+    public function destroyAction()
+    {
+        $params = $this->request->getPost();
+
+        $this->model->destroy($params);
+
+        // カテゴリ一覧へリダイレクト
+        header('Location: ./?controller=menu&action=index');
+        exit();
     }
 }
--- public_html/views/menu/new.tpl
+++ public_html/views/menu/new.tpl
@@ -2,10 +2,27 @@
 
 {include file='../templates/header.tpl' icon='tag' title='メニュー管理' action='追加'}
 
-<form class='form' action='?controller=menu&action=create' method='post'>
+<form class='form' action='?controller=menu&action=create&menu_id={$menu.menu_id}' method='post'>
   <p class='form__item'>
     <label class='form__item--label' for='name'>メニュー名</label>
-    <input class='form__item--input' id='name' name='name' type='text'>
+    <input class='form__item--input' id='name' name='name' type='text' value='{$menu.name}'>
+  </p>
+  {* TODO: <select>タグでカテゴリ名選択 *}
+  <p class='form__item'>
+    <label class='form__item--label' for='name'>カテゴリ</label>
+    <input class='form__item--input' id='name' name='category_id' type='text' value='{$menu.category_id}'>
+  </p>
+  <p class='form__item'>
+    <label class='form__item--label' for='name'>価格</label>
+    <input class='form__item--input' id='name' name='price' type='text' value='{$menu.price}'>
+  </p>
+  <p class='form__item'>
+    <label class='form__item--label' for='name'>画像</label>
+    <input class='form__item--input' id='name' name='image' type='text' value='{$menu.image}'>
+  </p>
+  {* ユーザに変更されると困るが、変更に必要な情報なのでhiddenにしておく *}
+  <p class='form__hidden-item'>
+    <input class='form__item--input' name='menu_id' type='text' value='{$menu.menu_id}'>
   </p>
   <p class='form__item'>
     <input class='form__item--button' type='submit' value='メニュー追加'>
