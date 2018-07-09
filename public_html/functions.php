73e1ee20a4d5ece325bc117df9966aaf1a077f5c
yamasy1549 <sanae@yamasy.info>
2018-07-09 14:33:19 +0900

    バリデート用関数定義

--- public_html/functions.php
+++ public_html/functions.php
@@ -2,3 +2,31 @@
 function h($str) {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+function validate(...$params) {
+  if (in_array(false, array_map('boolval', $params))) {
+    echo "error";
+  } else {
+    return true;
+  }
+}
+
+function isid($str) {
+  return preg_match("/^[1-9][0-9]?$/", $str);
+}
+
+function isnumber($str) {
+  return preg_match("/^[0-9]+$/", $str);
+}
+
+function isnumber_data($str) {
+  return preg_match("/^[1-5]+$/", $str);
+}
+
+function isdate($str) {
+  return preg_match("/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/", $str);
+}
+
+function isbool($str) {
+  return preg_match("/^[0-1]$/", $str);
+}
